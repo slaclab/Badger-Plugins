f18f494c0c1dbf2daa0cfefc78c9403ef9c5d035
michaellans <mlans@slac.stanford.edu>
2024-07-31 11:12:14 -0700

    applied black formatting with ufmt

--- environments/lcls/fel/observables.py
+++ environments/lcls/fel/observables.py
@@ -1,13 +1,15 @@
-
 import logging
 import time
-from badger.stats import percent_80
 from typing import List
-from badger.errors import BadgerEnvObsError
+
 import numpy as np
+from badger.errors import BadgerEnvObsError
+from badger.stats import percent_80
 
 
-def get_intensity_n_loss(hxr : bool, points : int, loss_pv : str, fel_channel : str, interface):
+def get_intensity_n_loss(
+    hxr: bool, points: int, loss_pv: str, fel_channel: str, interface
+):
     """
     Gets intensity and loss data from interface and returns mean, median, p80, and standard deviation for intensity and p80 for loss.
 
@@ -43,25 +45,26 @@ def get_intensity_n_loss(hxr : bool, points : int, loss_pv : str, fel_channel : 
     The last 120 entries correspond to pulse energies over past 1 second.
     """
 
-    logging.info(f'Get value of {points} points')
+    logging.info(f"Get value of {points} points")
 
     # Sleep for a while to get enough data
     try:
-        rate = interface.get_value('EVNT:SYS0:1:LCLSBEAMRATE')
-        logging.info(f'Beam rate: {rate}')
+        rate = interface.get_value("EVNT:SYS0:1:LCLSBEAMRATE")
+        logging.info(f"Beam rate: {rate}")
         nap_time = points / (rate * 1.0)
     except Exception as e:
         nap_time = 1
         logging.warn(
-            'Something went wrong with the beam rate calculation. Let\'s sleep 1 second.')
-        logging.warn(f'Exception was: {e}')
+            "Something went wrong with the beam rate calculation. Let's sleep 1 second."
+        )
+        logging.warn(f"Exception was: {e}")
 
     time.sleep(nap_time)
 
     if hxr:
-        PV_gas = f'GDET:FEE1:{fel_channel}:ENRCHSTCUHBR'
+        PV_gas = f"GDET:FEE1:{fel_channel}:ENRCHSTCUHBR"
     else:  # SXR
-        PV_gas = 'EM1K0:GMD:HPS:milliJoulesPerPulseHSTCUSBR'
+        PV_gas = "EM1K0:GMD:HPS:milliJoulesPerPulseHSTCUSBR"
     try:
         results_dict = interface.get_values([PV_gas, loss_pv])
         intensity_raw = results_dict[PV_gas][-points:]
@@ -82,11 +85,12 @@ def get_intensity_n_loss(hxr : bool, points : int, loss_pv : str, fel_channel : 
         if hxr:  # we don't have scalar input for HXR
             raise BadgerEnvObsError
         else:
-            gas = interface.get_value('EM1K0:GMD:HPS:milliJoulesPerPulse')
+            gas = interface.get_value("EM1K0:GMD:HPS:milliJoulesPerPulse")
 
             return gas, gas, gas, 0, 0
-    
-def get_loss(points : int, loss_pv : str, interface):  # if only loss is observed
+
+
+def get_loss(points: int, loss_pv: str, interface):  # if only loss is observed
     """
     Gets loss data from the beamline interface and returns the 80th percentile of the data.
 
@@ -107,17 +111,18 @@ def get_loss(points : int, loss_pv : str, interface):  # if only loss is observe
         80th percentile of loss data
 
     """
-    logging.info(f'Get value of {points} points')
+    logging.info(f"Get value of {points} points")
 
     try:
-        rate = interface.get_value('EVNT:SYS0:1:LCLSBEAMRATE')
-        logging.info(f'Beam rate: {rate}')
+        rate = interface.get_value("EVNT:SYS0:1:LCLSBEAMRATE")
+        logging.info(f"Beam rate: {rate}")
         nap_time = points / (rate * 1.0)
     except Exception as e:
         nap_time = 1
         logging.warn(
-            'Something went wrong with the beam rate calculation. Let\'s sleep 1 second.')
-        logging.warn(f'Exception was: {e}')
+            "Something went wrong with the beam rate calculation. Let's sleep 1 second."
+        )
+        logging.warn(f"Exception was: {e}")
 
     time.sleep(nap_time)
 
@@ -131,10 +136,11 @@ def get_loss(points : int, loss_pv : str, interface):  # if only loss is observe
     except Exception:  # we don't have scalar input for loss
         raise BadgerEnvObsError
 
-def is_pulse_intensity_observed(observable_names : List[str]):
+
+def is_pulse_intensity_observed(observable_names: List[str]):
     """
     Returns the number of observables in observable_names which start with 'pulse_intensity'
-    
+
     Parameters
     ----------
     observable_names : list
@@ -145,5 +151,6 @@ def is_pulse_intensity_observed(observable_names : List[str]):
     int
         number of items in parameter list starting with 'pulse_instensity'
     """
-    return len([name for name in observable_names if
-                name.startswith('pulse_intensity')])
+    return len(
+        [name for name in observable_names if name.startswith("pulse_intensity")]
+    )
